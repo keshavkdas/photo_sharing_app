f17a6fa5b68488301c45ad7b4c5e80846ab7e420
keshavkdas <94295810+keshavkdas@users.noreply.github.com>
2024-07-02 15:58:16 +0530

    Update login.php

--- login.php
+++ login.php
@@ -10,7 +10,6 @@ $dbname = "photo_sharing_app"; // Database name
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $user = $_POST['username'];
     $pass = $_POST['password'];
@@ -18,10 +17,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Validate user input
     $user = htmlspecialchars($user);
     $pass = htmlspecialchars($pass);
-
-    // Debugging
-    echo "Username: " . $user . "<br>";
-    echo "Password: " . $pass . "<br>";
 
     // Prepare and bind
     $stmt = $conn->prepare("SELECT id, username, password FROM users WHERE username = ?");
@@ -31,16 +26,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if ($result->num_rows > 0) {
         $row = $result->fetch_assoc();
-        // Debugging
-        echo "Fetched Username: " . $row['username'] . "<br>";
-        echo "Fetched Password Hash: " . $row['password'] . "<br>";
 
-        if (($pass==$row['password'])) {
+        if ($pass == $row['password']) {
             // Password is correct, set session variables
             $_SESSION['id'] = $row['id'];
             $_SESSION['username'] = $row['username'];
             $_SESSION['loggedIn'] = 'true';
-            print_r($_SESSION['loggedIn']);
             // Redirect to the upload page or any other page after successful login
             header("Location: upload.html");
             exit();
@@ -50,7 +41,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             exit();
         }
     } else {
-         $_SESSION['error'] = "Invalid username or password.";
+        $_SESSION['error'] = "Invalid username or password.";
         header("Location: login.php");
         exit();
     }
@@ -58,6 +49,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Close the statement and connection
     $stmt->close();
     $conn->close();
-
 }
 ?>
