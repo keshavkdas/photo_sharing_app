cefea37f7cb317cebbb8e602b139b8e79bf68d0e
keshavkdas <94295810+keshavkdas@users.noreply.github.com>
2024-07-08 16:29:27 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,9 +1,4 @@
 <?php
-require 'vendor/autoload.php';
-
-use Aws\SecretsManager\SecretsManagerClient; 
-use Aws\Exception\AwsException;
-
 session_start();
 
 //header("Content-Security-Policy: default-src 'self'; script-src 'self' https://cdnjs.cloudflare.com; style-src 'self' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com;");
@@ -15,51 +10,13 @@ $dbname = "photo_sharing_app"; // Database name
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Function to get secret from AWS Secrets Manager
-function getSecret($secretName) {
-    $client = new SecretsManagerClient([
-        'version' => 'latest',
-        'region' => 'ap-south-1' 
-    ]);
-
-    try {
-        $result = $client->getSecretValue([
-            'SecretId' => $secretName,
-        ]);
-
-        if (isset($result['SecretString'])) {
-            return json_decode($result['SecretString'], true);
-        }
-    } catch (AwsException $e) {
-        error_log($e->getMessage());
-    }
-
-    return null;
-}
-
-// Retrieve reCAPTCHA keys from AWS Secrets Manager
-$secret = getSecret('captcha'); 
-$siteKey = $secret['recaptcha_site_key'];
-$secretKey = $secret['recaptcha_secret_key'];
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $user = $_POST['username'];
     $pass = $_POST['password'];
-    $recaptchaResponse = $_POST['g-recaptcha-response'];
 
     // Validate user input
     $user = htmlspecialchars($user);
     $pass = htmlspecialchars($pass);
-
-    // Verify reCAPTCHA
-    $response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$secretKey&response=$recaptchaResponse");
-    $responseKeys = json_decode($response, true);
-
-    if (intval($responseKeys["success"]) !== 1) {
-        $error = "Please complete the CAPTCHA.";
-        header("Location: login.html");
-        exit();
-    }
 
     // Prepare and bind
     $stmt = $conn->prepare("SELECT id, username, password FROM users WHERE username = ?");
