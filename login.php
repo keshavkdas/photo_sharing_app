57e7e622044ca2048e717ca7a638686ab1051396
keshavkdas <94295810+keshavkdas@users.noreply.github.com>
2024-07-08 16:04:54 +0530

    Update login.php

--- login.php
+++ login.php
@@ -40,7 +40,6 @@ function getSecret($secretName) {
 // Retrieve reCAPTCHA keys from AWS Secrets Manager
 $secret = getSecret('captcha'); 
 $siteKey = $secret['recaptcha_site_key'];
-echo $sitekey;
 $secretKey = $secret['recaptcha_secret_key'];
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
