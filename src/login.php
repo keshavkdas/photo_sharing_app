08338012a8b41fe10cd293b4fd317c5deaa6763c
keshavkdas <94295810+keshavkdas@users.noreply.github.com>
2024-07-12 15:06:40 +0530

    Update login.php

--- src/login.php
+++ src/login.php
@@ -3,7 +3,7 @@ session_start();
 
 //header("Content-Security-Policy: default-src 'self'; script-src 'self' https://cdnjs.cloudflare.com; style-src 'self' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com;");
 
-$servername = "localhost";
+$servername = "172.31.41.155";
 $username = "root"; // Database username
 $password = "Keshav@123"; // Database password
 $dbname = "photo_sharing_app"; // Database name
